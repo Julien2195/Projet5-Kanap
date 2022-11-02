4249063eafec64e1742275c5dc7b35db68174300
Julien2195 <82829066+Julien2195@users.noreply.github.com>
2022-11-02 19:25:08 +0100

    ajout titre + description

--- front/js/products.js
+++ front/js/products.js
@@ -1,33 +1,42 @@
 
-   
 
-function getProductById(){
+
+function getProductById() {
 
     //Recherche de l'URL via window.location
     const queryString = window.location.search;
-    
+
     //On coupe le ? avec la methode slice pour recuperer seulement l'id 
     let myID = queryString.slice(1)
-       // ON APPELLE LA FONCTION 
-    
-        fetch(`http://localhost:3000/api/products/${myID}`).then(response => response.json()).then(data => {
-    
-            const item = document.querySelector('.item__img')
-    
-    
-            let img = document.createElement('img')
-    
-    
-    
-            img.src = data.imageUrl
-            img.alt = data.altTxt
-    
-    
-            item.append(img)
-    
-    
-    
-        }
-        )
+    // ON APPELLE LA FONCTION 
+
+    fetch(`http://localhost:3000/api/products/${myID}`).then(response => response.json()).then(data => {
+
+
+        const item = document.querySelector('.item__img')
+        const titleH1 = document.querySelector('#title')
+        const price = document.querySelector('#price')
+        const img = document.createElement('img')
+        const description = document.querySelector('#description')
+      
+
+        titleH1.innerText = data.name
+        price.innerText = data.price
+        description.innerText = data.description
+
+
+        img.src = data.imageUrl
+        img.alt = data.altTxt
+        item.append(img)
+        
+
+        
+
+
     }
-        getProductById()
+    )
+}
+getProductById()
+
+
+fetch(`http://localhost:3000/api/products/`).then(response => response.json()).then(data => (console.log(data)))
