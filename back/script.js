1c10a09efe6b95248cd71a01d0dd3b95992db260
Julien2195 <82829066+Julien2195@users.noreply.github.com>
2022-11-02 16:34:23 +0100

    ajout fetch + ajout des images et des canapés de l'api
    - ajout de l'api sur le server
    - ajout des images + descriptions des canapés
    - redirection de l'id de chaque canapés sur la page products.js

---
+++ back/script.js
@@ -0,0 +1,33 @@
+
+
+fetch('http://localhost:3000/api/products').then(response => response.json()).then(data => {
+
+    let items = document.querySelector('.items')
+
+
+    for (let i = 0; i < data.length; i++) {
+
+        let newTitreCarte = document.createElement('h3')
+        let newBodyCarte = document.createElement('p')
+        let img = document.createElement("img");
+        let article = document.createElement('article')
+        let lien = document.createElement('a')
+
+
+        newTitreCarte.innerText = data[i].name
+        newBodyCarte.innerText = data[i].description
+
+        img.src = data[i].imageUrl
+        img.alt = data[i].altTxt
+        lien.href = `./product.html?${data[i]._id}`
+        
+        article.append(img)
+        article.append(newTitreCarte)
+        article.append(newBodyCarte)
+
+        items.append(lien)
+        lien.append(article)
+
+    }
+
+})
