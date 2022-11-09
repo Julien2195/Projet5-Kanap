38f0a8c0e0b74bc360e97990546689a55c717143
Julien2195 <82829066+Julien2195@users.noreply.github.com>
2022-11-09 16:39:34 +0100

    ajout prix, couleur, description

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,50 +1,54 @@
-
-
 fetch(`http://localhost:3000/api/products`)
   .then((response) => response.json())
   .then((data) => {
+    let panier = getPanier();
+
+    for (let i = 0; i < panier.length; i++) {
+      let item = panier[i];
+      let foundID = data.find((p) => p._id === item.id);
+      if (foundID != undefined) {
+
+        //QUERYSELECTOR
+        const idClass = document.querySelector("#cart__items");
+        //ARTICLE
+        const article = document.createElement("article");
+        const classCart_item = article.classList.add("cart__item");
+        article.setAttribute("data-id", "product-ID");
+        article.setAttribute("data-color", "product-color");
+        //DIV,Hn
+        const div1 = document.createElement("div");
+        const div2 = document.createElement("div");
+        const div3 = document.createElement("div");
+       
+        const h2 = document.createElement("h2");
+        const pColor =document.createElement("p")
+        const pPrice =document.createElement("p")
+
+        //CART  IMAGE
+         const img = document.createElement("img");
+        div1.classList.add("cart__item__img");
+        img.src = foundID.imageUrl;
+        img.alt = foundID.altTxt;
+        //CART ITEM CONTENT
+        div2.classList.add("cart__item__content");
+        div3.classList.add("cart__item__content__description");
+        h2.textContent = data[i].description;
+       pColor.textContent = panier[i].color;
+       pPrice.textContent = `${data[i].price} €`
+
+        /////////////////////////////////
+        idClass.append(article);
+        article.append(div1);
+        div1.append(img);
+        article.append(div2);
+        article.append(div3);
+        div3.append(h2);
+        div3.append(pColor)
+        div3.append(pPrice)
+      }
+    }
     
- let panier = getPanier()
- console.log(panier[0].color);
- for(let i =0; i<panier.length; i++){
-  let item = panier[i]
-  let foundID = data.find((p)=> p._id === item.id)
-  console.log(data);
-  if(foundID != undefined){
-    const idClass = document.querySelector("#cart__items");
-    //ARTICLE
-    const article = document.createElement("article");
-    const classCart_item = article.classList.add("cart__item");
-    article.setAttribute("data-id", "product-ID");
-    article.setAttribute("data-color", "product-color");
-  //DIV 
-    const div1 = document.createElement("div");
-    const div2 = document.createElement("div");
-    const div3 =  document.createElement('div')
-    const img = document.createElement('img')
-    // H,P ...
-    const h2 = document.createElement('h2')
-  //CART  IMAGE
-    div1.classList.add("cart__item__img");
-
-   //CART ITEM CONTENT
-  div2.classList.add("cart__item__content")
-    div3.classList.add("cart__item__content__description")
-
-    img.src=foundID.imageUrl
-
-    idClass.append(article);
-    article.append(div1);
-    div1.append(img)
-    article.append(div2);
-    article.append(div3)
-    div3.append(h2) 
-  }
- }
-  // h2.innerHTML= `${}`
-    //////////////////////////////////////////////////////////////////////////////:
-    
- });
+  });
 
 // A SUPPRIMER
 fetch("http://localhost:3000/api/products")
